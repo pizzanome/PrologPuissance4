e9c30eef06162192ad2adaed45ac6478ba9a1e2d
Grel Hugo <grelhugo@gmail.com>
2022-12-09 10:34:49 +0100

    Temps moyen par jouer

--- testIAs.pl
+++ testIAs.pl
@@ -14,6 +14,8 @@
 
 :- dynamic joueurCourant/2.
 :- dynamic autreJoueur/2.
+:- dynamic tempsJoueur/2.
+:- dynamic nbCoups/2.
 
 %%%%%%%%%%%%%%%%%%%%%%%
 %% Prédicats publics %%
@@ -42,6 +44,13 @@ runTest(NbIterations,IA1,IA2) :-
 
 runTest2(NbIterations,IA1,IA2) :-
 	NbIterationsParIA is NbIterations//2,
+
+	%Mise à zéro du temps écoulé pour les joueurs ainsi que leurs nombres de coups.
+	assert(tempsJoueur(IA1,0)),
+	assert(tempsJoueur(IA2,0)),
+	assert(nbCoups(IA1,0)),
+	assert(nbCoups(IA2,0)),
+
 	runTestIAXEnPremier(NbIterationsParIA,IA1,IA2,0,NbFoisIA1GagneEnCommencant,0,NbFoisIA1PerdEnCommencant,0,NbEgaliteIA1),
 	runTestIAXEnPremier(NbIterationsParIA,IA2,IA1,0,NbFoisIA2GagneEnCommencant,0,NbFoisIA2PerdEnCommencant,0,NbEgaliteIA2),
 	typeJoueur(IA1,TypeIA1),
@@ -60,7 +69,9 @@ runTest2(NbIterations,IA1,IA2) :-
 
 	write(Stream, TypeIA2), write(Stream, ' en commençant : a gagné '), write(Stream, NbFoisIA2GagneEnCommencant),write(Stream, ' fois et a perdu '),write(Stream, NbFoisIA2PerdEnCommencant),write(Stream, ' fois.\n'),
 	write(Stream, TypeIA1), write(Stream, ' en commençant : a gagné '), write(Stream, NbFoisIA1GagneEnCommencant),write(Stream, ' fois et a perdu '),write(Stream, NbFoisIA1PerdEnCommencant),write(Stream, ' fois.\n'),
-	
+	write(Stream, 'Il y a eu '), write(Stream, NbEgaliteIA2),write(Stream, ' égalites pour IA2'),nl,
+	write(Stream, '\nIl y a eu '), write(Stream, NbEgaliteIA1),write(Stream, ' egalites pour IA1'),nl,
+
 	write(TypeIA2), write(' en commençant : a gagné '), write(NbFoisIA2GagneEnCommencant),write(' fois et a perdu '),write(NbFoisIA2PerdEnCommencant),write(' fois.'),
 	nl,
 	write('Il y a eu '), write(NbEgaliteIA2),write(' egalites pour IA2'),nl,
@@ -107,40 +118,26 @@ runTestWithStatisticsShort(NbIterations,IA1,IA2) :-
 	runTest2(NbIterations,IA1,IA2),
 	statistics(walltime, [End,_]),
 	Time is End - Start,
+	%calcul du temps moyen par coups
+	tempsJoueur(IA1, TempsIA1),
+	tempsJoueur(IA2, TempsIA2),
+	nbCoups(IA1, NbCoupsIA1),
+	nbCoups(IA2, NbCoupsIA2),
+	MeanTimeIA1 is TempsIA1/NbCoupsIA1,
+	MeanTimeIA2 is TempsIA2/NbCoupsIA2,
+
+	write(Stream, "\nTemps moyen par coups pour IA1 : "), write(Stream, MeanTimeIA1), write(Stream, " ms"),
+	write(Stream, "\nTemps moyen par coups pour IA2 : "), write(Stream, MeanTimeIA2), write(Stream, " ms"),
+
 	statistics,
 	statistics(cputime,CpuTime),
 	statistics(threads, Threads),
 	%affichage des statistiques
-	write(Stream, 'Execution Time : '), write(Stream, Time), write(Stream, ' ms.'),
+	write(Stream, '\nExecution Time : '), write(Stream, Time), write(Stream, ' ms.'),
 	write(Stream, '\nNb de thread actifs : '), write(Stream, Threads),
 	write(Stream, '\nCpuTime : '), write(Stream,CpuTime), write(Stream, ' s'),
 	write(Stream, '\n\n'),
 	close(Stream).
-
-%Test avec runTestWithStatisticsShort de toutes les IA contre toutes les autres, avec 10 parties par match
-% testAllIA :-
-% 	runTestWithStatisticsShort(10,2,3),
-% 	runTestWithStatisticsShort(10,2,4),
-% 	runTestWithStatisticsShort(10,2,5),
-% 	runTestWithStatisticsShort(10,2,6),
-% 	runTestWithStatisticsShort(10,2,7),
-% 	runTestWithStatisticsShort(10,2,8),
-% 	runTestWithStatisticsShort(10,3,4),
-% 	runTestWithStatisticsShort(10,3,5),
-% 	runTestWithStatisticsShort(10,3,6),
-% 	runTestWithStatisticsShort(10,3,7),
-% 	runTestWithStatisticsShort(10,3,8),
-% 	runTestWithStatisticsShort(10,4,5),
-% 	runTestWithStatisticsShort(10,4,6),
-% 	runTestWithStatisticsShort(10,4,7),
-% 	runTestWithStatisticsShort(10,4,8),
-% 	runTestWithStatisticsShort(10,5,6),
-% 	runTestWithStatisticsShort(10,5,7),
-% 	runTestWithStatisticsShort(10,5,8),
-% 	runTestWithStatisticsShort(10,6,7),
-% 	runTestWithStatisticsShort(10,6,8),
-% 	runTestWithStatisticsShort(10,7,8).
-
 
 %%%%%%%%%%%%%%%%%%%%%%
 %% Prédicats privés %%
@@ -152,7 +149,9 @@ runTestIAXEnPremier(NbIterations,IA1,IA2,NbIA1GagneIni,NbIA1GagneFin,NbIA2GagneI
 	init,
 	assert(joueurCourant(rouge,IA1)), % rouge = IA1
 	assert(autreJoueur(jaune,IA2)),
+
 	jeu(PartieNulle),
+
 	joueurCourant(CouleurIAGagnante,_),
 	incrementerGagnant(PartieNulle,CouleurIAGagnante,NbIA1GagneIni,NbIA1GagneFin1,NbIA2GagneIni,NbIA2GagneFin1, NbDrawIni, NbDrawFin1),
 	NbIterations2 is NbIterations-1,
@@ -169,10 +168,28 @@ jeu(PartieNulle) :-
 	tour(PartieNulle).
 
 tour(PartieNulle) :-
+	%Mesurer le temps de jeu de chaque tour
+	statistics(walltime, [Start,_]),
+
 	joueurCourant(CouleurJCourant,TypeJoueur),
 	obtenirCoup(CouleurJCourant,TypeJoueur,Coup),
 	placerJeton(Coup,Y,CouleurJCourant),
+
+	statistics(walltime, [End,_]),
+	Time is End - Start,
+
+	%Mesurer le temps de jeu du tour et incrémenter le nombre de coups de 1
+	tempsJoueur(TypeJoueur, TempsAncien),
+	nbCoups(TypeJoueur, NbCoupsAncien),
+	TempsNouveau is TempsAncien + Time,
+	NbCoupsNouveau is NbCoupsAncien + 1,
+	retractall(tempsJoueur(TypeJoueur,_)),
+	retractall(nbCoups(TypeJoueur,_)),
+	assert(tempsJoueur(TypeJoueur,TempsNouveau)),
+	assert(nbCoups(TypeJoueur,NbCoupsNouveau)),
+
 	testFin(Coup,Y,CouleurJCourant, PartieNulle).
+
 
 testFin(Coup,Y,CouleurJCourant,PartieNulle) :-
 	gagne(Coup,Y,CouleurJCourant),
